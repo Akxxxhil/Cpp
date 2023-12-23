6bb77e4c9ab05d50e8b24b561ba20f016aa0af26
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2023-12-23 22:01:17 +0530

    Day 1

---
+++ Day 1/arearect.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+   int l;
+   int b;
+   cin>>l;
+   cin>>b;
+   
+     int area=l*b;
+    cout<<area<<endl;
+
+    return 0;
+}
---
+++ Day 1/question1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int x=5;
+  char y='A';
+
+  y=x+y;
+  cout<<y<<endl;  // the answer will be F
+
+    
+}
